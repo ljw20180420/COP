9dba9bd8ed3edb701e14d766ad6ca73172a4d68b
ljw <ljw2017@sjtu.edu.cn>
2026-04-18 18:15:18 +0800

    Upload COP to huggingface space.

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from common_ai.config import get_app_parser, get_train_parser
+
+from AI.gradio_fn import MyGradioFn
+
+app_parser = get_app_parser()
+train_parser = get_train_parser()
+
+cfg = app_parser.parse_path("AI/app.yaml")
+
+MyGradioFn(cfg, train_parser).launch()
---
+++ scripts/formal/space.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os
+import pathlib
+
+# change to the project folder
+os.chdir(pathlib.Path(__file__).resolve().parent.parent.parent)
+
+from huggingface_hub import create_repo, upload_file, upload_folder, whoami
+
+username = whoami()["name"]
+common_ai_path = f"{os.environ['PYTHONPATH']}/common_ai"
+create_repo(
+    repo_id=f"{username}/COP", repo_type="space", exist_ok=True, space_sdk="gradio"
+)
+upload_folder(
+    repo_id=f"{username}/COP",
+    folder_path=common_ai_path,
+    path_in_repo="common_ai",
+    repo_type="space",
+    ignore_patterns=["__pycache__/*", "**/__pycache__", "*.yaml"],
+    delete_patterns="*",
+)
+upload_folder(
+    repo_id=f"{username}/COP",
+    folder_path="AI",
+    path_in_repo="AI",
+    repo_type="space",
+    ignore_patterns=["__pycache__/*", "**/__pycache__/*", "dataset/*", "*.pkl"],
+    delete_patterns="*",
+)
+upload_file(
+    path_or_fileobj="app.py",
+    path_in_repo="app.py",
+    repo_id=f"{username}/COP",
+    repo_type="space",
+)
+upload_file(
+    path_or_fileobj="requirements_space.txt",
+    path_in_repo="requirements.txt",
+    repo_id=f"{username}/COP",
+    repo_type="space",
+)
